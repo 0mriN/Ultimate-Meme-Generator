9d487b0bf4fbfb05c7d322465bdaaf889acea5c0
Omri Nadav <omrinadav15@gmail.com>
2024-06-13 13:46:03 +0300

    Add Adjusments

--- js/memeController.js
+++ js/memeController.js
@@ -1,16 +1,17 @@
 'use strict'
 
 function renderMeme(text) {
-    getMeme(text)
+    var currMeme = getMeme()
+    setLineTxt(text)
     gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height);
     coverCanvasWithImg(gSelctedImg)
-    drawText(gMeme.text,gMeme.textX,gMeme.textY)
+    drawText(currMeme.lines[currMeme.selectedLineIdx].txt, gMeme.textX, gMeme.textY)
 }
 
 function drawText(text, x = 120, y = 50) {
     gCtx.lineWidth = 2
-    gCtx.strokeStyle = 'blue'
-    gCtx.fillStyle = 'black'
+    gCtx.strokeStyle = 'gMeme.lines[0].color'
+    gCtx.fillStyle = 'gMeme.lines[0].color'
     gCtx.font = '40px Arial'
     gCtx.textAlign = 'center'
     gCtx.textBaseline = 'middle'
--- js/memeService.js
+++ js/memeService.js
@@ -1,15 +1,28 @@
 'use strict'
-var gMeme 
-
-function getMeme(text){
-    gMeme = {
-        selectedImg:gSelctedImg,
-        text:text,
-        textX:120,
-        textY:50
-    }
+var gImgs = [
+    { id: 1, url: 'meme-imgs/meme-imgs (square)/1.jpg', keywords: ['angry'] },
+    { id: 2, url: 'meme-imgs/meme-imgs (square)/2.jpg', keywords: ['cute,puppy,puppies'] }
+]
+var gMeme = {
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'Your Text',
+            size: 20,
+            color: 'red'
+        }
+    ]
 }
 
-function setLineTxt(){
-    
+
+function getMeme() {
+    return gMeme
+}
+
+function setLineTxt(text) {
+    var currMeme = getMeme()
+    var selectedLineIdx = currMeme.selectedLineIdx
+    currMeme.lines[selectedLineIdx].txt = text
+    // renderMeme()
 }
