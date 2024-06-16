e3ea1882d8915f00288da7574934141aaba3ad81
Omri Nadav <omrinadav15@gmail.com>
2024-06-16 18:47:35 +0300

    Add filter and styling

--- js/galleryController.js
+++ js/galleryController.js
@@ -2,22 +2,22 @@
 var gImgs = [
     { id: 1, url: 'meme-imgs/meme-imgs (square)/1.jpg', keywords: ['angry', 'men'] },
     { id: 2, url: 'meme-imgs/meme-imgs (square)/2.jpg', keywords: ['cute', 'animal'] },
-    { id: 2, url: 'meme-imgs/meme-imgs (square)/3.jpg', keywords: ['cute', 'baby', 'animal'] },
-    { id: 2, url: 'meme-imgs/meme-imgs (square)/4.jpg', keywords: ['cute', 'animal'] },
-    { id: 2, url: 'meme-imgs/meme-imgs (square)/5.jpg', keywords: ['cute', 'angry', 'baby'] },
-    { id: 2, url: 'meme-imgs/meme-imgs (square)/6.jpg', keywords: ['funny', 'men'] },
-    { id: 2, url: 'meme-imgs/meme-imgs (square)/7.jpg', keywords: ['cute', 'baby', 'funny'] },
-    { id: 2, url: 'meme-imgs/meme-imgs (square)/8.jpg', keywords: ['men'] },
-    { id: 2, url: 'meme-imgs/meme-imgs (square)/9.jpg', keywords: ['cute', 'baby', 'funny'] },
-    { id: 2, url: 'meme-imgs/meme-imgs (square)/10.jpg', keywords: ['men', 'funny'] },
-    { id: 2, url: 'meme-imgs/meme-imgs (square)/11.jpg', keywords: ['men', 'funny'] },
-    { id: 2, url: 'meme-imgs/meme-imgs (square)/12.jpg', keywords: ['men'] },
-    { id: 2, url: 'meme-imgs/meme-imgs (square)/13.jpg', keywords: ['men'] },
-    { id: 2, url: 'meme-imgs/meme-imgs (square)/14.jpg', keywords: ['men'] },
-    { id: 2, url: 'meme-imgs/meme-imgs (square)/15.jpg', keywords: ['men', 'funny'] },
-    { id: 2, url: 'meme-imgs/meme-imgs (square)/16.jpg', keywords: ['men', 'funny'] },
-    { id: 2, url: 'meme-imgs/meme-imgs (square)/17.jpg', keywords: ['men'] },
-    { id: 2, url: 'meme-imgs/meme-imgs (square)/18.jpg', keywords: ['funny'] }
+    { id: 3, url: 'meme-imgs/meme-imgs (square)/3.jpg', keywords: ['cute', 'baby', 'animal'] },
+    { id: 4, url: 'meme-imgs/meme-imgs (square)/4.jpg', keywords: ['cute', 'animal'] },
+    { id: 5, url: 'meme-imgs/meme-imgs (square)/5.jpg', keywords: ['cute', 'angry', 'baby'] },
+    { id: 6, url: 'meme-imgs/meme-imgs (square)/6.jpg', keywords: ['funny', 'men'] },
+    { id: 7, url: 'meme-imgs/meme-imgs (square)/7.jpg', keywords: ['cute', 'baby', 'funny'] },
+    { id: 8, url: 'meme-imgs/meme-imgs (square)/8.jpg', keywords: ['men'] },
+    { id: 9, url: 'meme-imgs/meme-imgs (square)/9.jpg', keywords: ['cute', 'baby', 'funny'] },
+    { id: 10, url: 'meme-imgs/meme-imgs (square)/10.jpg', keywords: ['men', 'funny'] },
+    { id: 11, url: 'meme-imgs/meme-imgs (square)/11.jpg', keywords: ['men', 'funny'] },
+    { id: 12, url: 'meme-imgs/meme-imgs (square)/12.jpg', keywords: ['men'] },
+    { id: 13, url: 'meme-imgs/meme-imgs (square)/13.jpg', keywords: ['men'] },
+    { id: 14, url: 'meme-imgs/meme-imgs (square)/14.jpg', keywords: ['men'] },
+    { id: 15, url: 'meme-imgs/meme-imgs (square)/15.jpg', keywords: ['men', 'funny'] },
+    { id: 16, url: 'meme-imgs/meme-imgs (square)/16.jpg', keywords: ['men', 'funny'] },
+    { id: 17, url: 'meme-imgs/meme-imgs (square)/17.jpg', keywords: ['men'] },
+    { id: 18, url: 'meme-imgs/meme-imgs (square)/18.jpg', keywords: ['funny'] }
 ]
 
 function renderGallery() {
@@ -76,11 +76,45 @@ function toggleGallery() {
     galleryLi.classList.add('li-active');
 }
 
-
 function toggleAbout() {
     var aboutLi = document.querySelector('.about');
     aboutLi.classList.add('li-active');
     var galleryLi = document.querySelector('.gallery');
     galleryLi.classList.remove('li-active');
 
+}
+
+function onSetFilter(filterText) {
+    var input = document.querySelector('.search-container input[type="search"]')
+    input.value = filterText
+    filterText = filterText.toLowerCase()
+    var filteredImgs = gImgs.filter(function (img) {
+        return img.keywords.some(function (keyword) {
+            return keyword.toLowerCase().includes(filterText)
+        })
+    })
+    renderFilteredGallery(filteredImgs)
+
+    var clickedButton = document.querySelector('.filter-container button[data-clicks]:hover')
+    if (clickedButton) {
+        var currentClicks = parseInt(clickedButton.getAttribute('data-clicks')) || 0
+        currentClicks++
+        clickedButton.style.fontSize = (14 + currentClicks) + 'px'
+        clickedButton.setAttribute('data-clicks', currentClicks);
+    }
+}
+
+function renderFilteredGallery(imgs) {
+    var galleryContainer = document.querySelector('.gallery-container')
+    galleryContainer.innerHTML = ''
+
+    imgs.forEach(function (img) {
+        var elImg = document.createElement('img')
+        elImg.src = img.url
+        elImg.alt = 'meme-Image'
+        elImg.addEventListener('click', function () {
+            onSelectImg(this)
+        })
+        galleryContainer.appendChild(elImg)
+    })
 }
